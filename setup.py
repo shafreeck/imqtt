846f0fa0b7b9264cb66ddd2b68180a2f11f8b01c
Shafreeck Sea <shafreeck@gmail.com>
2016-07-21 13:03:22 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     author = 'Shafreeck Sea',
     author_email = 'shafreeck@gmail.com',
     url = 'https://github.com/shafreeck/imqtt',
-    download_url = 'https://github.com/shafreeck/imqtt/tarball/0.1', 
     keywords = ['mqtt', 'ipython', 'python', 'imqtt', 'mqtt client'],
     classifiers = [],
     py_modules=['imqtt'],
