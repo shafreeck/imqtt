6a722a6334bf8232963e68b5b74d9046b1890dbf
Shafreeck Sea <shafreeck@gmail.com>
2016-07-23 19:54:43 +0800

    Fix missing modules

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 setup(
     name = 'imqtt',
-    version = '0.2.1',
+    version = '0.2.2',
     description = 'Iteractive MQTT packet manipulation shell based on IPython',
     author = 'Shafreeck Sea',
     author_email = 'shafreeck@gmail.com',
     url = 'https://github.com/shafreeck/imqtt',
     keywords = ['mqtt', 'ipython', 'python', 'imqtt', 'mqtt client'],
     classifiers = [],
-    py_modules=['imqtt'],
+    py_modules=['imqtt', 'ipython'],
     scripts= ['imqtt'],
     install_requires=[
         'IPython',
