ba4228fc955d160372dab15eb32d657c5aa0aa27
Shafreeck Sea <shafreeck@gmail.com>
2016-08-04 13:48:13 +0800

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 setup(
     name = 'imqtt',
-    version = '0.2.4',
+    version = '0.2.5',
     description = 'Iteractive MQTT packet manipulation shell based on IPython',
     author = 'Shafreeck Sea',
     author_email = 'shafreeck@gmail.com',
     url = 'https://github.com/shafreeck/imqtt',
     keywords = ['mqtt', 'ipython', 'python', 'imqtt', 'mqtt client'],
     classifiers = [],
-    py_modules=['imqtt', 'ipython'],
+    py_modules=['imqtt', 'ipshell'],
     scripts= ['imqtt'],
     install_requires=[
         'IPython',
